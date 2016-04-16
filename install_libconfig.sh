c1f11c94c137c133cd14137d601ceca6e66e34e9
Jack Lamberti <jal416@lehigh.edu>
2016-04-16 00:49:08 -0400

    And another

--- install_libconfig.sh
+++ install_libconfig.sh
@@ -2,7 +2,7 @@
 sudo apt-get install automake autoconf
 
 git clone https://github.com/hyperrealm/libconfig.git
-cd libconfig && ./configure && touch configure.ac aclocal.m4 configure Makefile.am Makefile.in && make
+cd libconfig && ./configure && touch configure.ac aclocal.m4 configure Makefile.am Makefile.in && make && sudo make install
 
 #wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++9_1.4.9-2_amd64.deb
 #sudo dpkg --install libconfig++9_1.4.9-2_amd64.deb
