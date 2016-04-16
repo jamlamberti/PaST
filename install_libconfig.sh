fdef068b587ec085b008371a44c5f8b0eb6892d0
Jack Lamberti <jal416@lehigh.edu>
2016-04-16 00:01:54 -0400

    Maybe install a deb

--- install_libconfig.sh
+++ install_libconfig.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo apt-get install automake
+#sudo apt-get install automake
 
-git clone https://github.com/hyperrealm/libconfig.git
-cd libconfig && ./configure && make
+#git clone https://github.com/hyperrealm/libconfig.git
+#cd libconfig && ./configure && make
+
+wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++9_1.4.9-2_amd64.deb
+sudo dpkg --install libconfig++9_1.4.9-2_amd64.deb
