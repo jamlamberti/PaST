5331ffc6d3a36f3053b246c921ad5410c385876d
Jack Lamberti <jal416@lehigh.edu>
2016-04-19 10:02:58 -0400

    Debugging which version of libconfig we are running

---
+++ src/libcfg_test.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <libconfig.h++>
+
+int main()
+{
+    std::cout << LIBCONFIGXX_VER_MAJOR << "." << LIBCONFIGXX_VER_MINOR << "." << LIBCONFIGXX_VER_REVISION << std::endl;    
+}
