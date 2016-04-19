57f3ee3aacbe2b5ea5cd542ec928321c9ff6d574
Jack Lamberti <jal416@lehigh.edu>
2016-04-19 10:41:54 -0400

    Trying this

--- install_libconfig.sh
+++ install_libconfig.sh
@@ -1,9 +1,8 @@
-!/bin/bash
+#!/bin/bash
 #sudo apt-get install automake autoconf
 
 #git clone https://github.com/hyperrealm/libconfig.git
 #cd libconfig && ./configure && touch configure.ac aclocal.m4 configure Makefile.am Makefile.in && make && sudo make install
-wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++9_1.4.9-2_amd64.deb
-wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++-dev_1.4.9-2_amd64.deb
-sudo dpkg --install libconfig++9_*_amd64.deb
-sudo dpkg --install libconfig++-dev*_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++9_1.4.9-2_amd64.deb http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++-dev_1.4.9-2_amd64.deb http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig9_1.4.9-2_amd64.deb http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig-dev_1.4.9-2_amd64.deb
+sudo dpkg --install *.deb
+#sudo dpkg --install libconfig++-dev*_amd64.deb
