2da0ae27175ada0c7c651cb1eb1e55f2fad8bcc7
Jack Lamberti <jal416@lehigh.edu>
2016-04-19 10:26:01 -0400

    Resync with install command

--- install_libconfig.sh
+++ install_libconfig.sh
@@ -5,4 +5,4 @@
 #cd libconfig && ./configure && touch configure.ac aclocal.m4 configure Makefile.am Makefile.in && make && sudo make install
 
 wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++-dev_1.4.9-2_amd64.deb
-sudo dpkg --install libconfig++9_1.4.9-2_amd64.deb
+sudo dpkg --install libconfig++*_amd64.deb
