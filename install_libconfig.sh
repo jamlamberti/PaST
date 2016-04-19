dcbd08948868cda335f68750472d201eed55f333
Jack Lamberti <jal416@lehigh.edu>
2016-04-19 10:20:59 -0400

    Switched to explicit dev version

--- install_libconfig.sh
+++ install_libconfig.sh
@@ -4,5 +4,5 @@
 #git clone https://github.com/hyperrealm/libconfig.git
 #cd libconfig && ./configure && touch configure.ac aclocal.m4 configure Makefile.am Makefile.in && make && sudo make install
 
-wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++9_1.4.9-2_amd64.deb
+wget http://mirrors.kernel.org/ubuntu/pool/main/libc/libconfig/libconfig++-dev_1.4.9-2_amd64.deb
 sudo dpkg --install libconfig++9_1.4.9-2_amd64.deb
